6b39266523d94789113ae6c40576d81ad9cfaf35
ErenYildiztekin <144628328+ErenYildiztekin@users.noreply.github.com>
2024-11-03 14:50:36 +0300

    Update ex12.c

--- ex12.c
+++ ex12.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    int number; 
+    long factorial = 1;
+    
+    printf("Faktoriyelini hesaplamak için bir sayı (0 ile 50 arasında) bir sayı giriniz: ");
+    scanf("%d", &number);
+    
+    if (number > 50 || number < 0) {
+        printf("Hata! Lütfen belirtilen aralıkta bir sayı giriniz.");
+        return 1;
+    }
+    
+    for(int i=1; i<=number;i++){
+        factorial *= i;
+    }
+    
+    printf("%d! = %ld\n", number, factorial);
+    
+    return 0;
+}
