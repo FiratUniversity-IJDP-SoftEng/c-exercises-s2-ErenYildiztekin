e86cac550b831bbc82bf18c61726c39ff084aee0
ErenYildiztekin <144628328+ErenYildiztekin@users.noreply.github.com>
2024-11-03 13:21:58 +0300

    Update ex06.c

--- ex06.c
+++ ex06.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+    int numbers[17];
+    int index = 0;
+    
+    for(int i=99; i>=51; i-=3) {
+        numbers[index++] = i;
+    }
+    
+    for(int i=0; i<17; i++){
+        printf("%i", numbers[i]);
+        if(i<16) {
+            printf(", ");
+        }
+    }
+    
+    return 0;
+}
