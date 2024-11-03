c700a501813448e4213455ac3df9902e241d46b2
ErenYildiztekin <144628328+ErenYildiztekin@users.noreply.github.com>
2024-11-03 14:17:16 +0300

    Update ex09.c

--- ex09.c
+++ ex09.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
 
+int main() {
+    int number;
+    int count = 0;
+    int sum = 0;
+    float average;
+    
+    while(1){
+        printf("Enter numbers (-1 to stop): ");
+        scanf("%d", &number);
+        if(number==-1){
+            break;
+        }
+        sum += number;
+        count++;
+    }
+    
+    if(count>0) {
+        average = sum / (float)count;
+    }else{
+        average = 0;
+    }
+    
+    printf("Count of numbers entered   : %d\n", count);
+    printf("Sum of numbers entered     : %d\n", sum);
+    printf("Average of numbers entered : %.2f\n", average);
+    
+    return 0;
+}
