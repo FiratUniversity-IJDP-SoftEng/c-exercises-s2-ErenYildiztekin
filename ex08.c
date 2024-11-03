0104b82f21eb3518a237cf0d59e2e8babd98e468
ErenYildiztekin <144628328+ErenYildiztekin@users.noreply.github.com>
2024-11-03 13:48:08 +0300

    Update ex08.c

--- ex08.c
+++ ex08.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main() {
+    int numbers[10];
+    int sum = 0;
+    float average;
+    
+    for(int i=0; i<10; i++){
+        printf("Enter the number %i: ", i+1);
+        scanf("%d", &numbers[i]);
+        sum += numbers[i];
+    }
+    
+    average = sum / 10.0;
+    
+    //Girilen sayıları gösteren döngü
+    for(int i=0; i<10; i++){
+        printf("Numbers entered: %d\n", numbers[i]);
+    }
+    
+    printf("Sum            : %d\n",sum);
+    printf("Average        : %f", average);
+    
+    return 0;
+}
